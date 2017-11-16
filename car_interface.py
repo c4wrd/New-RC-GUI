dac2dbbc67af1493a1982a7d6d4bd0acde4d6c10
SeanJungst <seanjungst@gmail.com>
2017-11-15 17:58:03 -0700

    Add files via upload

--- car_interface.py
+++ car_interface.py
@@ -21,14 +21,14 @@ class RCCar:
             (-100, 100) where negative numbers are reverse, and positive numbers are forward,
             and a value of 0 will stop the car
         """
-        raise NotImplementedError("Not implemented")
+        print("Set speed: %d" %speed)
 
     def set_direction(direction: int):
         """
         Sets the direction of the car.
 
         :param int direction: The direction to set the car towards, where direction is in
-        the range (-45, 45) which is the degrees to the left or right (left is negative, right is positive).
+        the range (-100, 100) where -100 turns the car fully to the left, and 100 turns the car fully to the right
         Setting the direction to 0 will drive the car forward
         """
-        raise NotImplementedError("Not implemented")
+        print("Set direection: %d" %direction)
